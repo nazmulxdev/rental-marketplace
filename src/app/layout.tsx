12094b79745b0ddae7f2c6690e51d5a2c695e823
Saif-Smran <23303079@iubat.edu>
2025-09-05 10:40:23 +0600

    Basic Nevbar Complite

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "../context/ThemeProvider";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider>{children}</ThemeProvider>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider>
+          {/* Global navigation */}
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
